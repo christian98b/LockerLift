81ebe21e6e61102be1db57ea58219698aac202bd
Christian Bruns <christianbruns1039@gmail.com>
2026-09-14 20:39:44 +0200

    chore(project): bump version to v1.3.0 (code 5)

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lockerlift.wear"
         minSdk = 30
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
